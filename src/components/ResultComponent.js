81029076f46af53485eb2380f599f3a207ccd679
deepthigontla99 <deepthigontla99@gmail.com>
2021-09-17 11:08:16 +0530

    commited assignment

---
+++ src/components/ResultComponent.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result" align="center">
+                <p>{result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default ResultComponent;
